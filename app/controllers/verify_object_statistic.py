66af3cf0402ddc2773d8ae680c70b713add847e0
wmsj100 <wmsj100@hotmail.com>
2020-09-03 12:05:49 +0800

    modify verify objects ok

--- app/controllers/verify_object_statistic.py
+++ app/controllers/verify_object_statistic.py
@@ -17,9 +17,31 @@ class VerifyObjectStatistic:
         self.__dest_data = {}
 
     def __restruct_sqlite_data(self, data):
+        """ 
+        restruct list to object 
+        data: [('name', 'owner', 'status')]
+        return: {'OE': {'name', 'owner', 'statu'}}
+        """
         logging.info(f'{data}')
+    
+    def __compare_object_data(self, object_name):
+        """
+        get source and dest data from sqlite
+        object_name: table
+        write source data to self._source_data
+        write dest data to self._dest_data
+        return Null
+        """
+        _source_data = self.__sqlite_db.sqlite_query_common_object_table(object_name, 'source')
+        _dest_data = self.__sqlite_db.sqlite_query_common_object_table(object_name, 'dest')
+        _source_data = self.__restruct_sqlite_data(_source_data)
+        _dest_data = self.__restruct_sqlite_data(_dest_data)
 
     def __compare_init(self):
-        _source_data = self.__sqlite_db.sqlite_query_common_object_table('function', 'source')
-        _source_data = self.__restruct_sqlite_data(_source_data)
+        """
+        compare object data
+        return Null
+        """
+        self.__compare_object_data('function')
+        # self.__compare_object_data('index')
 
