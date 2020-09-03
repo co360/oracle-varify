5ec09bae78e3eb9b9b1af094e09bbc8378c2f919
wmsj100 <wmsj100@hotmail.com>
2020-09-03 17:44:02 +0800

    export empty excel file ok

--- app/controllers/export_each_object_excel.py
+++ app/controllers/export_each_object_excel.py
@@ -1,6 +1,26 @@
-import openpyxl
+from openpyxl import Workbook
+from openpyxl.styles import Alignment
 import logging
+import os
+import datetime
+import shutil
 
 class ExportEachObjectExcel:
     def __init__(self):
-        logging.info('excel ok')
+        self.book = Workbook()
+        self.__excel_file_path_config()
+        self.__excel_save()
+    
+    def __excel_file_path_config(self):
+        date_str = datetime.datetime.now()
+        date_path = date_str.strftime("%Y-%m-%d_%H-%M-%S")
+        cur_dir = os.path.dirname(os.path.abspath('__file__'))
+        excel_dir = os.path.join(cur_dir, 'excel', date_path)
+
+        if os.path.exists(excel_dir):
+            shutil.rmtree(excel_dir)
+        os.makedirs(excel_dir)
+        self.file_path = os.path.join('excel', date_path, 'oracle_objects_statistic.xls')
+    
+    def __excel_save(self):
+        self.book.save(self.file_path)
