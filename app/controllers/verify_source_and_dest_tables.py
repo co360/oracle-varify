627246cbcb77b3861ce093921ea76489d397d31a
wmsj100 <wmsj100@hotmail.com>
2020-09-04 21:06:47 +0800

    get page info

--- app/controllers/verify_source_and_dest_tables.py
+++ app/controllers/verify_source_and_dest_tables.py
@@ -39,6 +39,9 @@ class VerifySourceAndDestTables:
     def __get_target_tables(self):
         tables = self.sqlite_db.sqlite_oracle_table_column_table_query()
         # logging.info(tables)
+        for table in tables:
+            pass
+            # self.__analysis_target_table_data(table)
         self.__analysis_target_table_data(tables[15])
 
     def __get_table_max_rows(self, table_name: str, num_rows: str):
@@ -78,7 +81,11 @@ class VerifySourceAndDestTables:
             last_page = varify_rows % self.default_per_page
             per_page = self.default_per_page
 
-        result = [per_page, pages, last_page]
+        result = {
+            'per_page': per_page,
+            'pages': pages,
+            'last_page': last_page
+        }
 
         logging.info(f'Table {table_name} page info is {result}')
         return result
@@ -86,12 +93,25 @@ class VerifySourceAndDestTables:
     def __analysis_target_table_data(self, table_data: list):
         """ analysis data get num_rows """
         table_name = table_data[1]
-        columns = table_data[2]
-        primarys = table_data[-2]
+        
         max_rows = table_data[3]
         varify_percent = table_data[-1]
         logging.info(table_data)
         max_rows = self.__get_table_max_rows(table_name, max_rows)
         varify_rows = self.__get_table_verify_rows(
             table_name, max_rows, varify_percent)
-        page_data = self.__get_table_per_pages(table_name, varify_rows)
+        page_obj = self.__get_table_per_pages(table_name, varify_rows)
+        last_page = page_obj['pages'] + 1
+
+        for page in range(1, last_page):
+            self.__query_oracle_table_order_by_primary(
+                table_data, page_obj['per_page'], page)
+        self.__query_oracle_table_order_by_primary(
+            table_data, page_obj['last_page'], last_page)
+
+    def __query_oracle_table_order_by_primary(self, table_data: list, per_page: int, page: int):
+        """ query oracle table order by primary """
+        columns = table_data[2]
+        primarys = table_data[-2]
+        logging.info(f'========== {per_page, page}')
+        
